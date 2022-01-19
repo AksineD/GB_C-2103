fe38683b045e283df775986756b4a96bab08c484
Denis Y <dyampolsky@yokohama-oht.com>
2022-01-19 16:59:01 +0200

    added code

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,7 +10,13 @@
     {
         private static void Main(string[] args)
         {
-           
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello, {name}, today is {date.ToString("dd.MM.yyyy")} ");
+
+            Console.WriteLine("Press 'ENTER' for exit");
+            Console.Read();
         }
     }
 }
